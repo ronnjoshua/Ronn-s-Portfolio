4554f5a85cad5c9928b1e16d6c7f7a32ebe7ac08
ronnjoshua <53202469+ronnjoshua@users.noreply.github.com.>
2024-08-14 13:26:05 +0800

    Modify Button

--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -2,34 +2,46 @@
 import React, { useState, useEffect } from "react";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 import { TextGenerateEffect2 } from "./ui/text-generate-effect2";
-import { cn } from "@/lib/utils";
 import { Boxes } from "./ui/background-boxes";
 import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
-const words = `Bringing Your Ideas to Life, One Line of Code at a Time.`;
-const introduction = "Hi, Im Ronn a Website Developer.";
+const words = `Ideas to Life, `;
+const introduction = `Ronn `;
+
+// `Bringing Your Ideas to Life, One Line of Code at a Time.`
 
 const Hero = () => {
-  const [showButton, setShowButton] = useState(false);
+  // const [showButton, setShowButton] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowButton(true);
-    }, 2300);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowButton(true);
+  //   }, 2300);
 
-    return () => clearTimeout(timer); // Cleanup the timeout if the component unmounts
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
     <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
       <Boxes />
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
       <TextGenerateEffect
-        words={words}
+        visibleText="Bringing Your"
+        animatedText={words}
+        postAnimatedText="One Line of Code at a Time."
         className="text-center text-[40] md:text-5xl lg:text-6xl z-20"
       />
-      <TextGenerateEffect2 words={introduction} className="z-20" />
-      <a href="#about">{showButton && <MagicButton />}</a>
+      <TextGenerateEffect2
+        visibleText="Hi, I'm "
+        animatedText={introduction}
+        postAnimatedText=" a Website Developer."
+        className="z-20"
+      />
+      <a className="mt-4" href="#about">
+        <MagicButton title="Show my work" icon={<FaLocationArrow/>} 
+        position="right"/>
+      </a>
     </div>
   );
 };
--- my-portfolio/components/ui/text-generate-effect2.tsx
+++ my-portfolio/components/ui/text-generate-effect2.tsx
@@ -4,62 +4,77 @@ import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const TextGenerateEffect2 = ({
-  words,
+  visibleText,
+  animatedText,
+  postAnimatedText,
   className,
   filter = true,
   duration = 0.5,
 }: {
-  words: string;
+  visibleText: string;
+  animatedText: string;
+  postAnimatedText?: string;
   className?: string;
   filter?: boolean;
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  let wordsArray = animatedText.split(" ");
+
   useEffect(() => {
-    animate(
-      "span",
-      {
-        opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
-      },
-      {
-        duration: duration ? duration : 1,
-        delay: stagger(0.2),
+    const loopAnimation = async () => {
+      for (let i = 0; i < 3; i++) {
+        await animate(
+          "span",
+          {
+            opacity: [0, 1],
+            filter: filter ? ["blur(10px)", "blur(0px)"] : "none",
+          },
+          {
+            duration: duration,
+            delay: stagger(0.35),
+          }
+        );
+
+        // Only fade out if it's not the last iteration
+        if (i < 2) {
+          await animate("span", { opacity: 0 }, { duration: 1, delay: 1.8 });
+        }
       }
-    );
+    };
+
+    loopAnimation();
   }, [animate, duration, filter]);
 
-  const renderWords = () => {
+  const renderAnimatedWords = () => {
     return (
-      <motion.div ref={scope}>
-        {wordsArray.map((word, idx) => {
-          return (
-            <motion.span
-              key={word + idx}
-              className={`${
-                idx >= 2 && idx <= 2
-                  ? "text-purple-500"
-                  : "dark:text-gray-100 text-black"
-              } opacity-0`}
-              style={{
-                filter: filter ? "blur(10px)" : "none",
-              }}
-            >
-              {word}{" "}
-            </motion.span>
-          );
-        })}
-      </motion.div>
+      <motion.span ref={scope}>
+        {wordsArray.map((word, idx) => (
+          <motion.span
+            key={word + idx}
+            className={`${
+              idx >= 0 && idx <= 2
+                ? "text-purple-500"
+                : "dark:text-gray-100 text-black"
+            } opacity-0`}
+            style={{
+              filter: filter ? "blur(10px)" : "none",
+            }}
+          >
+            {word}{" "}
+          </motion.span>
+        ))}
+      </motion.span>
     );
   };
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
-          {renderWords()}
-        </div>
+      <div className="dark:text-white text-black text-2xl leading-snug tracking-wide">
+        {/* Render the visible text, animated words, and post-animated text in a single line */}
+        <span>{visibleText} </span>
+        {renderAnimatedWords()}
+        {postAnimatedText && <span> {postAnimatedText}</span>}
       </div>
     </div>
   );
