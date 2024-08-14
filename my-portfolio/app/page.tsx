ef234952953673c4b23c6b091adb1baabd622158
ronnjoshua <53202469+ronnjoshua@users.noreply.github.com.>
2024-08-14 11:20:56 +0800

    Add Button in Header

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,24 +1,10 @@
-import { Boxes } from "@/components/ui/background-boxes";
-import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
-import { TextGenerateEffect2 } from "@/components/ui/text-generate-effect2";
+import Hero from "@/components/Hero";
 import Image from "next/image";
-
-const words = `Bringing Your Ideas to Life, One Line of Code at a Time.`;
-const introduction = "Hi, Im Ronn a Website Developer.";
 
 export default function Home() {
   return (
-    <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
-      <Boxes />
-      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-      <TextGenerateEffect
-        words={words}
-        className="text-center text-[40] md:text-5xl lg:text-6xl z-20"
-      />
-      <TextGenerateEffect2
-        words={introduction}
-        className="z-20"
-      />
+    <div>
+    <Hero/>
     </div>
   );
 }
--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -1,21 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { TextGenerateEffect2 } from "./ui/text-generate-effect2";
 import { cn } from "@/lib/utils";
 import { Boxes } from "./ui/background-boxes";
+import MagicButton from "./ui/MagicButton";
 
-const words = `Hi! I'm Ronn, a passionate Web Developer dedicated to creating dynamic and engaging online experiences.`;
+const words = `Bringing Your Ideas to Life, One Line of Code at a Time.`;
+const introduction = "Hi, Im Ronn a Website Developer.";
 
 const Hero = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowButton(true);
+    }, 2300);
+
+    return () => clearTimeout(timer); // Cleanup the timeout if the component unmounts
+  }, []);
+
   return (
     <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+      <Boxes />
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
       <TextGenerateEffect
         words={words}
-        className={cn(
-          "md:text-4xl text-xl text-white relative z-20 text-center"
-        )}
+        className="text-center text-[40] md:text-5xl lg:text-6xl z-20"
       />
-      <Boxes />
+      <TextGenerateEffect2 words={introduction} className="z-20" />
+      <a href="#about">{showButton && <MagicButton />}</a>
     </div>
   );
 };
